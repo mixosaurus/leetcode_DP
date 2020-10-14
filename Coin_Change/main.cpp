a6aa5d86ecbeeb94cc42485cb005a0aefa58b2e8
snake <lcpdfk@163.com>
2020-10-14 11:12:09 +0800

    322. 零钱兑换

--- Coin_Change/main.cpp
+++ Coin_Change/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //322. ¡„«Æ∂“ªª
 int main() {
 	Solution solution;
-	vector<int>coins = { 2,5,10,1 };
+	vector<int>coins = { 2,5,10 };
 	int amount = 27;
 	cout << solution.coinChange(coins, amount) << endl;
 	system("pause");
