066059d22f474ccb216cd889e71cec705497ab15
snake <lcpdfk@163.com>
2020-10-14 11:16:21 +0800

    322. 零钱兑换

--- Coin_Change/Solution.cpp
+++ Coin_Change/Solution.cpp
@@ -2,12 +2,12 @@
 #include <algorithm>
 
 int Solution::coinChange(vector<int>& coins, int amount) {
-    //以一个达不到是数值来表示正无穷
+    //以一个达不到的数值来表示正无穷
     int Max = amount + 1;
     vector<int> dp(amount + 1, Max);
     dp[0] = 0;
     for (int i = 1; i <= amount; ++i) {
-        //状态转义方程：dp(i)=min(dp(i−面值1),dp(i−面值2),...,dp(i−面值n))+1
+        //状态转移方程：dp(i)=min(dp(i−面值1),dp(i−面值2),...,dp(i−面值n))+1
         //求出每一个dp[i]
         for (int coin : coins) {
             //若i<coin，什么都不做，此时dp[i]仍为MAX
--- Coin_Change/Solution.h
+++ Coin_Change/Solution.h
@@ -1,7 +1,6 @@
 #pragma once
 #include<vector>
 #include<iostream>
-
 using namespace std;
 
 class Solution {
