1e924cfa1547f837ef0c7132947cc85a2e675f1c
snake <lcpdfk@163.com>
2020-10-23 16:56:01 +0800

    198. 打家劫舍

---
+++ House_Robber/Solution.cpp
@@ -0,0 +1,18 @@
+#include "Solution.h"
+
+int Solution::rob(vector<int>& nums) {
+	if (nums.empty()) {
+		return 0;
+	}
+    int size = nums.size();
+    if (size == 1) {
+        return nums.front();
+    }
+    int* dp = new int[size] {};
+    dp[0] = nums.front();
+    dp[1] = max(nums[0], nums[1]);
+    for (int i = 2; i < size; i++) {
+        dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+    }
+    return dp[size - 1];
+}
---
+++ House_Robber/main.cpp
@@ -0,0 +1,13 @@
+#include"Solution.h"
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//198. ¥Úº“ΩŸ…·
+int main() {
+	Solution solution;
+	vector<int> nums = { 1,2,3,1 };
+	cout << solution.rob(nums) << endl;
+	system("pause");
+	return 0;
+}
