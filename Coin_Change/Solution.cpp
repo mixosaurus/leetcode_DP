ebf636a756e9a07fa42c3ca6dd34a7eb7d42a1ec
snake <lcpdfk@163.com>
2023-08-02 23:00:30 +0800

    优化了注释

--- Coin_Change/Solution.cpp
+++ Coin_Change/Solution.cpp
@@ -7,13 +7,13 @@ int Solution::coinChange(vector<int>& coins, int amount) {
     vector<int> dp(amount + 1, Max);
     dp[0] = 0;
     for (int i = 1; i <= amount; ++i) {
+        //dp[i]为表示凑出金额i所需的最少硬币数量
         //状态转移方程：dp(i)=min(dp(i−面值1),dp(i−面值2),...,dp(i−面值n))+1
         //求出每一个dp[i]
         for (int coin : coins) {
             //若i<coin，什么都不做，此时dp[i]仍为MAX
             if (i>=coin) {
                 dp[i] = min(dp[i], dp[i - coin]);
-                //dp[i] = min(dp[i], dp[i - coin]+1);
             }
         }
         dp[i]++;
