4b8b5818c4696c3cb2d308aaa43dd1f794c7eccc
snake <lcpdfk@163.com>
2023-08-02 23:02:26 +0800

    322. 零钱兑换

--- Coin_Change/Solution.cpp
+++ Coin_Change/Solution.cpp
@@ -7,7 +7,7 @@ int Solution::coinChange(vector<int>& coins, int amount) {
     vector<int> dp(amount + 1, Max);
     dp[0] = 0;
     for (int i = 1; i <= amount; ++i) {
-        //dp[i]为表示凑出金额i所需的最少硬币数量
+        //dp[i]为凑出金额i所需的最少硬币数量
         //状态转移方程：dp(i)=min(dp(i−面值1),dp(i−面值2),...,dp(i−面值n))+1
         //求出每一个dp[i]
         for (int coin : coins) {
