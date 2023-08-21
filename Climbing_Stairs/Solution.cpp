e24f88e98b6b16c165e16f43dc05a601ea60ac5b
snake <lcpdfk@163.com>
2023-08-21 19:56:05 +0800

    70. 爬楼梯

--- Climbing_Stairs/Solution.cpp
+++ Climbing_Stairs/Solution.cpp
@@ -4,7 +4,7 @@
 // 勿混淆台阶数和方案数量，dp[i]不等于dp[i-2]+2，dp[i-2]+2的+2实际上是台阶数而非方案数，+2即混淆了台阶数和方案数
 int Solution::climbStairs(int n) {
     vector<int> dp(n + 1, 0);
-    // 无意义，仅占位
+    // 第从第0级到第0级无意义，仅占位
     dp[0] = 1;
     dp[1] = 1;
     for (int i = 2; i <= n; i++) {
