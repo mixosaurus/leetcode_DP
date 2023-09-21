863eb10a9d657e0e724f228ff904e14a630aa0eb
snake <lcpdfk@163.com>
2023-09-21 20:39:35 +0800

    416. 分割等和子集

--- Partition_Equal_Subset_Sum/Solution.cpp
+++ Partition_Equal_Subset_Sum/Solution.cpp
@@ -3,7 +3,9 @@
 // 可视作一个01背包问题，即从数组中选出数字，放入一个容量为数组一半的背包中，数字大小为体积，无价值属性
 bool Solution::canPartition(vector<int>& nums) {
     int sum = std::accumulate(nums.begin(), nums.end(), 0);
-    if (sum % 2 != 0) return false;
+    if (sum % 2 != 0) {
+        return false;
+    }
     sum = sum / 2;
     vector<int> dp(sum + 1, 0);
     for (int i = 0; i < nums.size(); i++) {
